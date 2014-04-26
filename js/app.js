c5e998e726d5a9c1b762508176bd7facfff0e90e
Marcus Saad <mv.nsaad@gmail.com>
2014-04-26 19:41:03 -0300

    #2 - Implements twitter sharing and finishes general structure. L10N pending

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 (function() {
-
-  var startbutton  = document.querySelector('#takephoto'),
+                   //Please, get your own as this is limited to 50 uploads an hour! https://imgur.com/register/api_anon
+  var 
+      startbutton  = document.querySelector('#takephoto'),
       video        = document.querySelector('#video'),
       flash        = document.querySelector('#flash'),
       canvas       = document.querySelector('#canvas'),
       vidcontainer = document.querySelector('#videocontainer'),
       tabs         = document.getElementsByClassName('tab'),
-      photo        = document.querySelector('#quadro'),
-      timers       = $("span[id$='timer']");
+      adjective    = document.querySelector('#adjective'),
+      urlfield     = document.querySelector('#imgur'),
+      timers       = $("span[id$='timer']"),
+      shareImage;
       
 
   var ctx    = canvas.getContext('2d'),
@@ -29,15 +32,18 @@
       imgheight = 606;
   img.src = 'media/images/frame.png';
     
-  
   setstate(state);
 
   function init() {
+
+    timers[0].hidden = true;
+    timers[1].hidden = true;
+    timers[2].hidden = true;
+
     navigator.getMedia = ( navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
                            navigator.mozGetUserMedia ||
                            navigator.msGetUserMedia);
-
     navigator.getMedia(
       {
         video: true,
@@ -50,15 +56,20 @@
           var vendorURL = window.URL || window.webkitURL;
           video.src = vendorURL ? vendorURL.createObjectURL(stream) : stream;
         }
+        timers[0].hidden = false;
+        timers[1].hidden = false;
+        timers[2].hidden = false;
         video.play();
       },
       function(err) {
-        console.log("An error occured! " + err);
+        console.log("No camera has been found or access to it has been denied! ");
+        tabs[1].className = "tab tab-inactive";
+        tabs[0].className = "tab tab-active";
+        setstate('intro');
       }
     );
   }
 
-  
  function setstate(newstate) {
     state = newstate;
     document.body.className = newstate;
@@ -71,10 +82,58 @@
     ctx.restore();
     ctx.scale(1, 1);
     ctx.drawImage(img, 606 - imgwidth, 606 - imgheight, imgwidth, imgheight);
+    adjective.hidden = true;
+    appendAdjective();
+
 
     if (callback)
       callback();
   }
+
+  function upload() {
+    var head = /^data:image\/(png|jpeg);base64,/,
+        fd = new FormData(),
+        xhr = new XMLHttpRequest();
+
+    setstate('sharing');
+
+    shareImage = canvas.toDataURL('image/png', 1).replace(head, '');
+
+    fd.append('image', shareImage);
+
+    xhr.open('POST', 'https://api.imgur.com/3/image.json');
+
+    xhr.addEventListener('error', function(ev) {
+      console.log('Upload Error!');
+    }, false);
+
+    xhr.addEventListener('load', function(ev) {
+      try {
+        var links = JSON.parse(xhr.responseText);
+        urlfield.innerHTML = links.data.link;
+        urlfield.href = links.data.link;
+        setstate('uploaded');
+      } catch(e) {
+        console.log('Upload Error!' + e);
+      }
+    }, false);
+
+    xhr.setRequestHeader("Authorization", "Client-ID a41e1ecad14d783");
+    xhr.send(fd);
+  }
+
+
+function shareOrRetake(){
+  document.getElementById("timers").hidden = true;
+}
+  /* Event Handlers */
+
+function appendAdjective(){
+  word = $('#myfirefox').val()
+  ctx.font="22px Open Sans";
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillText(word,383,537);
+}
 
 function timer(time,update,complete) {
     var start = new Date().getTime();
@@ -88,24 +147,18 @@ function timer(time,update,complete) {
     },100); 
 }
 
-
-function shareOrRetake(){
-  document.getElementById("timers").hidden = true;
-}
-  /* Event Handlers */
-
-
 video.addEventListener('play', function(ev){
     if (!streaming) {
       console.log(video.clientHeight);
       finalheight = video.clientHeight / (video.clientWidth/width);
-      video.setAttribute('width', 535);
+      video.setAttribute('width', 539);
       video.setAttribute('height', 470);
       canvas.width = width;
       canvas.height = height;
-      ctx.drawImage(img, 606 - imgwidth, 606 - imgheight, imgwidth, imgheight);
+      ctx.drawImage(img, 607 - imgwidth, 606 - imgheight, imgwidth, imgheight);
       streaming = true;
       vidcontainer.classname = 'playing';
+      adjective.hidden = false;
     }
   }, false);
 
@@ -118,13 +171,12 @@ startbutton.addEventListener('click', function(ev){
     if (state === 'reviewing'){
       setstate('picture');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 606 - imgwidth, 606 - imgheight, imgwidth, imgheight);
-      
+      ctx.drawImage(img, 606 - imgwidth, 606 - imgheight, imgwidth, imgheight);  
+
     }
 
     tabs[1].className = "tab tab-active";
     tabs[0].className = "tab tab-inactive";
-    
 
   }, false);
 
@@ -159,7 +211,8 @@ mediumtimer.addEventListener('click', function(ev){
       timers[0].hidden = false;
       timers[2].hidden = false;
       timers[1].children[1].innerHTML = "5 Seconds";
-      takepicture();
+      setstate('reviewing');
+      takepicture(shareOrRetake);
     }
   );
 }, false);
@@ -176,13 +229,29 @@ longtimer.addEventListener('click', function(ev){
       timers[0].hidden = false;
       timers[1].hidden = false;
       timers[2].children[1].innerHTML = "10 Seconds";
-      takepicture();
+      setstate('reviewing');
+      takepicture(shareOrRetake);
     }
   );
 }, false);
 
- retake.addEventListener('click', function(ev){
+retake.addEventListener('click', function(ev){
   $('#takephoto').click();
+  adjective.removeAttribute('hidden');
+}, false);
+
+share.addEventListener('click', function(ev){
+  tabs[2].className = "tab tab-active";
+  tabs[1].className = "tab tab-inactive";
+  video = null;
+  upload();
+}, false);
+
+
+twittershare.addEventListener('click', function(ev){
+  var textToTweet = "Meu Firefox nunca esteve tão " + $('#myfirefox').val() + ". " + "#Firefox #FirefoxBrasil " + urlfield.href ; 
+  var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(textToTweet);
+  window.open(twtLink,'_blank');
 }, false);
 
 })();
